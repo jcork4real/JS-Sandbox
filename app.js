6fa0a2a02133486ebac9fb23f0f2bbaae07618da
Jarone McCorkle <60796768+jcork4real@users.noreply.github.com>
2022-09-03 12:33:23 -0600

    Upload from workspace

--- app.js
+++ app.js
@@ -1,17 +1,15 @@
-const name = 'Jarone';
-const age = 30;
-const job = 'Web Developer';
-const city = 'Miami';
-let html;
+//create some arrays
 
-//Template Strings/Literals (es6)
-html = `
-    <ul>
-    <li>Name: ${name}</li>
-    <li>Age: ${age}</li>
-    <li>Job: ${job}</li>
-    <li>City: ${city}</li>
-    </ul>
-`;
 
-document.body.innerHTML = html;
+
+//Prompt
+
+
+val = window.navigator;
+
+window.console.log(val);
+
+
+
+
+
