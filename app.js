421ffb409abfdabf267f551706f14dcbdc2ffce5
Jarone McCorkle <60796768+jcork4real@users.noreply.github.com>
2022-09-04 01:25:50 -0600

    Codewars Practice

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
-//create some arrays
+function sumArray(array) {
+  if (array == null) {
+    return 0;
+  } else if (array.length < 2) {
+    return 0;
+  } else {
+    array = array.sort(function (a, b) {
+      return a - b;
+    });
+    var total = 0;
+    for (var i = 1; i < array.length - 1; i++) {
+      total += array[i];
+    }
+    return total;
+  }
+}
 
+const array = [5, 4, 3, 2, 1];
 
-
-//Prompt
-
-
-val = window.navigator;
-
-window.console.log(val);
-
-
-
-
-
+console.log(sumArray(array));
